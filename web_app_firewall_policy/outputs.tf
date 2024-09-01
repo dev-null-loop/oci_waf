1ef99cf977d487d020b3824e720998fccfc2f8c8
dev-null-loop <dev.null.loop@gmail.com>
2024-09-01 15:48:56 +0300

    waf_policy:added output

--- web_app_firewall_policy/outputs.tf
+++ web_app_firewall_policy/outputs.tf
@@ -1,10 +1,4 @@
-# locals {
-#   waf_policy_ids = {
-#     for c in oci_waf_web_app_firewall_policy.this :
-#     c.display_name => c.id
-#   }
-# }
-
-# output "waf_policy_ids" {
-#   value = local.waf_policy_ids
-# }
+output "id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the WebAppFirewallPolicy."
+  value       = oci_waf_web_app_firewall_policy.this.id
+}
